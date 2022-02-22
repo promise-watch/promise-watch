be2499290ee5677d299f081c219304e366a414f1
Jason Raimondi <jason@raimondi.us>
2022-02-21 19:59:50 -0800

    feat: add smtp notifier

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,18 +1,26 @@
-import "dotenv/config"
+import "dotenv/config";
 
 import { executeJobs, ExecuteOptions } from "@js-watcher/core";
 import { PushoverNotifier } from "@js-watcher/pushover";
-import { SlackNotifier } from "@js-watcher/slack";
+// import { SlackNotifier } from "@js-watcher/slack";
+// import { SmtpNotifer } from "@js-watcher/smtp";
 
 const options: ExecuteOptions = {
   dir: __dirname,
   errorNotifiers: [
     new PushoverNotifier(process.env.PUSHOVER_USER_KEY!, process.env.PUSHOVER_API_KEY!),
-    new SlackNotifier(process.env.SLACK_WEBHOOK_URL!),
-  ]
-}
+    // new SlackNotifier(process.env.SLACK_WEBHOOK_URL!),
+    // new SmtpNotifer({
+    //   host: process.env.SMTP_HOST,
+    //   port: Number(process.env.SMTP_PORT) || 587,
+    // }, {
+    //   to: process.env.SMTP_TO,
+    //   from: process.env.SMTP_FROM,
+    // }),
+  ],
+};
 
 executeJobs(options).catch(err => {
   console.error(err);
   process.exit(1);
-})
+});
---
+++ packages/smtp/src/main.ts
@@ -0,0 +1,21 @@
+import { Notifier, SendOptions } from "@js-watcher/core";
+import { createTransport, Transporter } from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import Mail from "nodemailer/lib/mailer";
+
+export class SmtpNotifer implements Notifier {
+  private readonly transporter: Transporter<SMTPTransport.SentMessageInfo>;
+
+  constructor(transport: SMTPTransport | SMTPTransport.Options | string, private readonly sendInfo: Mail.Options) {
+    this.transporter = createTransport(transport);
+  }
+
+  async send({ title, body }: SendOptions): Promise<void> {
+    await this.transporter.sendMail({
+      subject: title,
+      text: body,
+      html: `<p>${body}</p>`,
+      ...this.sendInfo,
+    });
+  }
+}
