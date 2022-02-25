86e58ac5c80e2c407965041a1cb86630d1d480b6
Jason Raimondi <jason@raimondi.us>
2022-02-24 17:37:41 -0800

    chore(example): tweak run error rate

--- example/runs/error-on-purpose.ts
+++ example/runs/error-on-purpose.ts
@@ -1,11 +1,11 @@
 import { RunOptions } from "@promise-watch/core";
 
 export const options: RunOptions = {
-  interval: 15,
+  interval: 10,
 };
 
 export async function run() {
-  if (Math.random() * 100 > 70) {
+  if (Math.random() * 100 > 30) {
     throw new Error("this is an error");
   } else {
     console.log("SUCCESS");
