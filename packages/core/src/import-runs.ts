bd32ac2199801f9c73b7128eb25079fc21ac2b3e
Jason Raimondi <jason@raimondi.us>
2022-02-25 21:34:09 -0800

    refactor: function cleanup

--- packages/core/src/import-runs.ts
+++ packages/core/src/import-runs.ts
@@ -4,7 +4,8 @@ import { RunPage } from "./types";
 
 export async function importRunsFromPath(globPath: string, dir: string): Promise<RunPage[]> {
   const files = await glob(globPath);
-  const imports = await Promise.all(files.map(f => import(resolve(dir, "../", f))));
+  const promises = files.map(f => resolve(dir, "../", f));
+  const imports = await Promise.all(promises.map(f => import(f)));
   return imports.map((page, idx) => ({
     name: page.name ?? files[idx].replace("runs/", ""),
     run: page.run,
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,17 +3,13 @@ import { ExecuteOptions } from "./types";
 import { importRunsFromPath } from "./import-runs";
 import { filterRunsWithoutRequiredFields } from "./filter-runs";
 
-export * from "./notifiers/console-notifier"
+export * from "./notifiers/console-notifier";
 export * from "./types";
 
 export async function executeJobs(options: ExecuteOptions) {
-  const {
-    dir,
-    notifiers = [],
-    globPath = "runs/**/*.{js,ts}",
-  } = options;
-  const files = await importRunsFromPath(globPath, dir);
-  const runs = filterRunsWithoutRequiredFields(files);
+  const { dir, notifiers = [], globPath = "runs/**/*.{js,ts}" } = options;
+  let runs = await importRunsFromPath(globPath, dir);
+  runs = filterRunsWithoutRequiredFields(runs);
   await Promise.allSettled(runs.map(run => recursivelyRun(run, notifiers)));
 }
 
