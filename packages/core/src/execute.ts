c62c7eff0457d9978acb927ca7328c8d51511b61
Jason Raimondi <jason@raimondi.us>
2022-03-02 23:00:29 -0800

    refactor: remove options dir requirement :tada:

--- packages/core/src/execute.ts
+++ packages/core/src/execute.ts
@@ -1,11 +1,11 @@
 import { recursivelyRun } from "./recursively-run";
 import { filterRunsWithoutRequiredFields } from "./filter-runs";
-import { ExecuteOptions } from "./types";
+import { ExecuteOptions, PromiseWatchOptions } from "./types";
 import { importRunsFromPath } from "./import-runs";
 
-export async function executeJobs(options: ExecuteOptions) {
-  const { dir, notifiers = [], globPath = "runs/**/*.{js,ts}" } = options;
-  let runs = await importRunsFromPath(globPath, dir);
+export async function executeJobs(options: PromiseWatchOptions | ExecuteOptions) {
+  const { notifiers = [], globPath = "runs/**/*.{js,ts}" } = options;
+  let runs = await importRunsFromPath(globPath);
   runs = filterRunsWithoutRequiredFields(runs);
   await Promise.allSettled(runs.map(run => recursivelyRun(run, notifiers)));
 }
